6e0d7e7902b7f359a2062316eb8bd014b41b9484
Christian Reyes <creyes.prog@gmail.com>
2024-06-11 22:57:23 -0400

    Fix layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import RootLayout from "./layout";
 
-export default function Home() {
+export default function Index() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <RootLayout>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        hello
+      </main>
+    </RootLayout>
   );
 }
