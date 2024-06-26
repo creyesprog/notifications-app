8dbaf96b49643ba6e43b6f40b8507eb5e1f1a679
Christian Reyes <creyes.prog@gmail.com>
2024-06-26 01:00:54 -0400

    Optimize dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,24 @@
+# Build stage
 FROM node:22-alpine AS build
-
-WORKDIR /work
+WORKDIR /build
 
 COPY package.json .
 RUN npm install
-
 COPY . .
+
+RUN npm run build
+
+# Run stage
+# TODO: Add security
+FROM node:22-alpine AS runner
+WORKDIR /app
+
+ENV NODE_ENV production
+
+COPY --from=build /build/public ./public
+COPY --from=build /build/.next/standalone ./
+COPY --from=build /build/.next/static ./.next/static
 
 EXPOSE 3000
 
-CMD ["npm", "run", "prod"]
+CMD HOSTNAME="0.0.0.0" node server.js
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'src')],
-      },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src")],
+  },
+  output: "standalone"
 };
 
 export default nextConfig;
