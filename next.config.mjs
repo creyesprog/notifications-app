8098a78b832bec485d3b7fbed102f4d2dc8ab73f
Christian Reyes <creyes.prog@gmail.com>
2024-06-25 21:16:43 -0400

    Use src path instead for next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import path from 'path';
-import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
@@ -8,7 +7,7 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     sassOptions: {
-        includePaths: [path.join(__dirname, 'app')],
+        includePaths: [path.join(__dirname, 'src')],
       },
 };
 
